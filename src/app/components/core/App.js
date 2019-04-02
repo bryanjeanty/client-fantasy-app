b4515ebc0c6cfc17261b194e2c4cc2bcaa87f73b
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-04-02 09:08:31 -0400

    modified react app

---
+++ src/app/components/core/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../../styles/core/App.css";
+
+class App extends Component {
+  render() {
+    return <h1>Dragon Stack from React</h1>;
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./app/components/core/App";
+
+render(<App />, document.getElementById("root"));
