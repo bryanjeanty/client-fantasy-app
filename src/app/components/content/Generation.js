5f38e9762a5da51cc0fde70ede60d9de0fe1007d
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-04-02 11:53:22 -0400

    changes made to generation component

--- src/app/components/content/Generation.js
+++ src/app/components/content/Generation.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 
 class Generation extends Component {
-  render() {
-    const generation = {
+  state = {
+    generation: {
       generationId: 999,
       expiration: "2020-05-01"
-    };
+    }
+  };
 
+  componentDidMount() {
+    this.fetchGeneration();
+  }
+
+  fetchGeneration = () => {
+    fetch("http://localhost:5000/generation").then(response =>
+      console.log("response", response)
+    );
+  };
+
+  render() {
+    const { generation } = this.state;
     return (
       <div>
         <h3>Generation {generation.generationId}. Expires on :</h3>
