04d61b945e2de3f99cf57c91f68a0ce1a1d0e5fa
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-04-05 20:13:03 -0400

    auth form component mostly complete

--- src/app/components/pages/AuthForm.js
+++ src/app/components/pages/AuthForm.js
@@ -1,8 +1,53 @@
 import React, { Component } from "react";
+import { Button, FormGroup, Input } from "reactstrap";
 
 class AuthForm extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  updateUsername = event => {
+    this.setState({ username: event.target.value });
+  };
+
+  updatePassword = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  signup = () => {
+    console.log("this.state", this.state);
+  };
+
+  login = () => {
+    console.log("this.state", this.state);
+  };
+
   render() {
-    return <div>Auth Form</div>;
+    return (
+      <div>
+        <h2>Dragon Stack</h2>
+        <FormGroup>
+          <Input
+            type="text"
+            value={this.state.username}
+            placeholder="username"
+            onChange={this.updateUsername}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            type="password"
+            value={this.state.password}
+            placeholder="password"
+            onChange={this.updatePassword}
+          />
+        </FormGroup>
+        <Button onClick={this.login}>Log In</Button>
+        <span> or </span>
+        <Button onClick={this.signup}>Sign Up</Button>
+      </div>
+    );
   }
 }
 
