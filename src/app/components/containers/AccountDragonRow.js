3b749460f43801711644aea939d7a9a74cc03cee
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-04-07 23:54:48 -0400

    implemented save and edit buttons to account dragons page

--- src/app/components/containers/AccountDragonRow.js
+++ src/app/components/containers/AccountDragonRow.js
@@ -1,13 +1,64 @@
 import React, { Component } from "react";
+import { Button } from "reactstrap";
 import DragonAvatar from "../content/DragonAvatar";
+import { BACKEND } from "../../../config";
 
 class AccountDragonRow extends Component {
+  state = {
+    nickname: this.props.dragon.nickname,
+    edit: false
+  };
+
+  updateNickname = event => {
+    this.setState({
+      nickname: event.target.value
+    });
+  };
+
+  toggleEdit = () => {
+    this.setState({ edit: !this.state.edit });
+  };
+
+  save = () => {
+    fetch(`${BACKEND.ADDRESS}/dragon/update`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        nickname: this.state.nickname,
+        dragonId: this.props.dragon.dragonId
+      })
+    })
+      .then(response => response.json())
+      .then(json => {
+        if (json.type === "error") {
+          alert(json.message);
+        } else {
+          this.toggleEdit();
+        }
+      })
+      .catch(error => alert(error.message));
+  };
+
+  get SaveButton() {
+    return <Button onClick={this.save}>Save</Button>;
+  }
+
+  get EditButton() {
+    return <Button onClick={this.toggleEdit}>Edit</Button>;
+  }
+
   render() {
     return (
       <div>
-        <h4>{this.props.dragon.nickname}</h4>
+        <input
+          type="text"
+          value={this.state.nickname}
+          onChange={this.updateNickname}
+          disabled={!this.state.edit}
+        />
         <br />
         <DragonAvatar dragon={this.props.dragon} />
+        {this.state.edit ? this.SaveButton : this.EditButton}
       </div>
     );
   }
